55e732ebcc7d787c95251714b852ed20a8c61198
jinw <jinw1212@naver.com>
2019-01-14 16:42:06 +0900

    v2 추가

---
+++ java script/ch02/index.js
@@ -0,0 +1,16 @@
+const firstName = "gildong";
+const lastName ="HONG"
+console.log(firstName);
+console.log(lastName);
+console.log(lastName+" " +firstName);
+const fullName = lastName+" "+firstName;
+console.log(fullName);
+
+
+const sendMoney ="money,10000,receiver jooho,sender,cos"
+console.log(sendMoney);
+
+console.log(sendMoney.split(","));
+
+console.log(1+"2");
+
---
+++ java script/ch03/index.js
@@ -0,0 +1,31 @@
+//변수의 타입
+
+
+const ex = 7;
+console.log(ex);
+
+
+
+
+
+
+
+
+
+
+
+
+
+let str="3";
+console.log(str);
+console.log(typeof str);
+console.log("----------");
+str =str+10;
+console.log(str);
+
+str = parseInt(str);
+console.log(str);
+console.log(typeof str);
+console.log("------------");
+
+
